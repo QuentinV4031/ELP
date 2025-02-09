ccecbce48d62ccb5452efc9ee0b4cf1b57d68d1f
QuentinV4031 <quentinv43120@yahoo.fr>
2025-02-09 12:35:33 +0100

    ajout du parser pour transformer l'input et du tracé avec Svg

--- Elm/src/Main.elm
+++ Elm/src/Main.elm
@@ -3,69 +3,137 @@ module Main exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput,onClick)
+import Html.Events exposing (onInput, onClick)
+import Parser exposing (..)
 
-
-
--- MAIN
-
-
-main : Program () Model Msg
+-- Main
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
-
-
--- MODEL
-
-
+-- Modèle
 type alias Model =
-  { texte : String
-  , programme : List String
-  }
+    { text : String
+    , erreurs : List DeadEnd
+    , result : List Instruction
+    }
 
+-- Définition des types d'instruction
+type Instruction
+    = Forward Int
+    | Left Int
+    | Right Int
+    | Repeat Int (List Instruction)
 
 init : Model
 init =
-  Model "" []
+    { text = ""
+    , erreurs = []
+    , result = []
+    }
 
-
-
--- UPDATE
-
-
+-- Message
 type Msg
-  = Prog String
-  | Ready
-
+    = In String
+    | ParseText
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Prog texte ->
-      { model | texte = texte }
+    case msg of
+        In texte ->
+          {model | text = texte}
+        ParseText ->
+            case read model.text of
+                Ok instructions ->
+                    { model | erreurs = [], result = instructions } -- Pas d'erreur, on met à jour result
 
-    Ready ->
-      parser model
+                Err parsingErrors ->
+                    { model | erreurs = parsingErrors, result = [] } -- Erreur, on met à jour erreurs et on vide result
 
 
-
--- VIEW
-
-
+-- Vue
 view : Model -> Html Msg
 view model =
-  div []
-    [ viewInput "text" "exemple: [Reapeat 100 [Forward 10]]" model.texte Prog
-    , button [ onClick Ready ] [ text "Click when ready!" ]
-    ]
-
+    div []
+        [ viewInput "text" "exemple: [Reapeat 100 [Forward 10]]: " model.text In
+        , button [ onClick ParseText ] [ text "Lire" ]
+        , div [] [ text "Résultat: ", text (instructionsToString model.result) ]
+        ]
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput t p v toMsg =
   input [ type_ t, placeholder p, value v, onInput toMsg ] []
 
+-- Convert a list of instructions to a string
+instructionsToString : List Instruction -> String
+instructionsToString instructions =
+    let
+        instructionToString instr =
+            case instr of
+                Forward n ->
+                    "Forward " ++ String.fromInt n
+                Left n ->
+                    "Left " ++ String.fromInt n
+                Right n ->
+                    "Right " ++ String.fromInt n
+                Repeat n instrs ->
+                    "Repeat " ++ String.fromInt n ++ instructionsToString instrs
+    in
+    "[" ++ String.join ", " (List.map instructionToString instructions) ++ "]"
 
-parser : Model -> Model
-parser model =
-  { model | programme = model.programme ++ [model.texte] }
+-- read
+
+read : String -> Result (List DeadEnd) (List Instruction)
+read input =
+    run parseInstructions input
+
+
+parseInstruction : Parser Instruction
+parseInstruction =
+    oneOf
+        [ succeed Forward
+            |. keyword "Forward"
+            |. spaces
+            |= int
+        , succeed Left
+            |. keyword "Left"
+            |. spaces
+            |= int
+        , succeed Right
+            |. keyword "Right"
+            |. spaces
+            |= int
+        , succeed Repeat
+            |. keyword "Repeat"
+            |. spaces
+            |= int
+            |. spaces
+            |= lazy (\_ -> sequence
+                { start = "["
+                , separator = ","
+                , end = "]"
+                , spaces = spaces
+                , item = parseInstruction
+                , trailing = Forbidden
+                })
+        ]
+
+parseInstructions : Parser (List Instruction)
+parseInstructions =
+    sequence
+        { start = "["
+        , separator = ","
+        , end = "]"
+        , spaces = spaces
+        , item = parseInstruction
+        , trailing = Forbidden
+        }
+
+
+parseLoop : List Instruction -> Parser (Step (List Instruction) (List Instruction))
+parseLoop acc =
+    oneOf
+        [ parseInstruction
+            |> andThen (\instr -> succeed (Loop (instr :: acc)))
+        , succeed (Done (List.reverse acc))
+        ]
+
